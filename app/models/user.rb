007f2d47e6a88f570a2c33ae14a253569bccf23f
João Paulo Motta <joao.paulo.fm@hotmail.com>
2020-09-14 19:50:39 -0300

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
-    has_many :quizzes
-    has_secure_password
+  has_many :quizzes
+
+  has_secure_password
+  validates name, presence: true, length: {maximum: 50}
+  validates password, presence: true, length: {minimum: 6}
+  VALID_EMAIL_FORMAT= /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+  validates email, presence: true, length: {maximum: 260}, format: { with: VALID_EMAIL_FORMAT}, uniqueness: {case_sensitive: false}
+  before_save { self.email = email.downcase }
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_14_224401) do
+ActiveRecord::Schema.define(version: 2020_09_14_224712) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2020_09_14_224401) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "matches", "players"

d6fd0e08175f67a77293de2fc41eeb4474b2d44b
João Paulo Motta <joao.paulo.fm@hotmail.com>
2020-09-14 20:33:38 -0300

    Add session login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   resources :users
-  resources :quizzes
+  get 'sign_in' => 'sessions#new'
+  post 'sign_in' => 'sessions#create'
+  delete 'sign_out' => 'sessions#destroy'
 
+  resources :quizzes
   match 'quiz' => 'quizzes#find_by_code', :via => :get
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

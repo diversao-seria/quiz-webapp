30d716f77a3e404c592c6be1944a2a071b0c4cc7
João Paulo Motta <joao.paulo.fm@hotmail.com>
2021-05-18 22:26:49 -0300

    Minor adjustments

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,5 @@ Rails.application.routes.draw do
   resources :quizzes
   match 'quiz' => 'quizzes#find_by_code', :via => :get
 
-  get 'show_question', to: 'questions#show_question'
-  get 'remove_question', to: 'questions#remove_question'
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

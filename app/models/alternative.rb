24c8f5b71b7379b5c02793449c47c36d7ed8545a
João Paulo Motta <joao.paulo.fm@hotmail.com>
2021-10-31 22:37:48 -0300

    Fix alternative correct check

--- app/models/alternative.rb
+++ app/models/alternative.rb
@@ -1,4 +1,11 @@
 class Alternative < ApplicationRecord
   belongs_to :question
 
+  after_initialize :set_alternative_correct
+
+  private
+  
+  def set_alternative_correct
+    self.correct ||= false
+  end
 end
